b64786eec6966a26eeebee582d82708b3100308f
Matan Banner <matan.banner@gmail.com>
2016-03-22 19:39:53 +0200

    finish

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,9 +1,20 @@
 class CommentsController < ApplicationController
+
+  http_basic_authenticate_with name: "matan", password: "secret", only: :destroy
+
   def create
     @article = Article.find(params[:article_id])
     @comment =  @article.comments.create(comment_params)
     redirect_to article_path(@article)
   end
+
+  def destroy
+    @article = Article.find(params[:article_id])
+    @comment = Comment.find(params[:id])
+    @comment.destroy
+    redirect_to article_path(@article)
+  end
+
 
   private
 
